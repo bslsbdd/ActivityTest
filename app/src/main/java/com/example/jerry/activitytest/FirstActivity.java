3fa7bf51f3d40ea1e3985392090d18717dc5eb49
Jerry <Jerryxucy@outlook.com>
2018-07-17 23:31:27 +0800

    Hidden Intent, Add Intent Category, Link Website, Process Http,Tel, Transfer Message, Get Result

--- app/src/main/java/com/example/jerry/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/jerry/activitytest/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.example.jerry.activitytest;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+    private static final String TAG = "FirstActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,12 @@ public class FirstActivity extends AppCompatActivity {
         Button button1 = (Button) findViewById(R.id.button_1);
         Button button2 = (Button) findViewById(R.id.button_2);
         Button button3 = (Button) findViewById(R.id.button_3);
+        Button button4 = (Button) findViewById(R.id.button_4);
+        Button button5 = (Button) findViewById(R.id.button_5);
+        Button button6 = (Button) findViewById(R.id.button_6);
+        Button button7 = (Button) findViewById(R.id.button_7);
+        Button button8 = (Button) findViewById(R.id.button_8);
+        Button button9 = (Button) findViewById(R.id.button_9);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +48,53 @@ public class FirstActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
                 startActivity(intent);
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.jerry.activitiytest.ACTION_START");
+                startActivity(intent);
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.jerry.activitiytest.ACTION_START");
+                intent.addCategory("com.example.jerry.activitiytest.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);
+                startActivity(intent);
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent,1);
             }
         });
     }
@@ -60,5 +117,18 @@ public class FirstActivity extends AppCompatActivity {
             default:
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case 1:
+                if (resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d(TAG, returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
